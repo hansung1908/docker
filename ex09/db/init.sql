fbaba6666de4757a1332fa90e4622a10053e2042
hansung1908 <hansung1908@naver.com>
2023-12-19 22:30:07 +0900

    spring + mysql 연결

---
+++ ex09/db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE product_tb (
+    id int auto_increment primary key,
+    name varchar(50),
+    price INT
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+insert into product_tb (name, price) values ('banana', 1000);
+insert into product_tb (name, price) values ('apple', 2000);
